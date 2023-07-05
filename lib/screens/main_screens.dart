041f1390baaa503c58ff3f478c2e4b6fed0f5593
mungyu <mungyu83@gmail.com>
2023-07-05 17:11:56 +0900

    메인 스크린 초기 설정

---
+++ lib/screens/main_screens.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainScreens extends StatefulWidget {
+  @override
+  State<MainScreens> createState() => _MainScreensState();
+}
+
+class _MainScreensState extends State<MainScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: const Text('MainScreens'),
+      ),
+    );
+  }
+}
