24dae37c43ed12f2c621d2c9befe02811d3fdfa3
mungyu <mungyu83@gmail.com>
2023-07-07 01:26:17 +0900

    홈 스크린 상단 AppBar 설정

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,46 @@
+import 'package:carrot_market_ui/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row( // 왼쪽 타이틀 영역
+          children: [
+            const Text('좌동'),
+            const SizedBox(width: 4.0,),
+            const Icon(
+              CupertinoIcons.chevron_down,
+              size: 15.0,
+            )
+          ],
+        ),
+        actions: [ // 오른쪽 액션 영역
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(CupertinoIcons.search),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(CupertinoIcons.list_dash),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(CupertinoIcons.bell),
+          )
+        ],
+        bottom: const PreferredSize( // 하단 영역
+            child: Divider(
+              thickness: 0.5,
+              height: 0.5,
+              color: Colors.grey,
+            ),
+            preferredSize: Size.fromHeight(0.5),
+        ),
+      ),
+      body: Container(),
     );
   }
 }
