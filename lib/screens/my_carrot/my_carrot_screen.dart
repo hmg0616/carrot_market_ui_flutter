d914deaa3d99adac69bd39962786e0c56ac97767
mungyu <mungyu83@gmail.com>
2023-07-09 21:27:24 +0900

    나의 당근 메뉴 기본 레이아웃 개발

--- lib/screens/my_carrot/my_carrot_screen.dart
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -1,10 +1,32 @@
+import 'package:carrot_market_ui/screens/my_carrot/components/my_carrot_header.dart';
 import 'package:flutter/material.dart';
 
 class MyCarrotScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('MyCarrotScreen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text("나의 당근"),
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.settings))
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
+        )
+      ),
+      body: ListView(
+        children: [
+          //MyCarrotHeader(),
+          const SizedBox(height: 8.0),
+          //CardIconMenu(iconMenuList: iconMenu1),
+          const SizedBox(height: 8.0),
+          //CardIconMenu(iconMenuList: iconMenu2),
+          const SizedBox(height: 8.0),
+          //CardIconMenu(iconMenuList: iconMenu3),
+        ],
+      ),
     );
   }
 }
