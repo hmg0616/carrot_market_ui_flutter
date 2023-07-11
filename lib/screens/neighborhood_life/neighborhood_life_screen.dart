1d933f14450b3be3e0411d87137da6df512dc855
mungyu <mungyu83@gmail.com>
2023-07-11 23:30:07 +0900

    동네 생활 메뉴 - 전체 레이아웃 구성

--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,10 +1,36 @@
+import 'package:carrot_market_ui/models/neighborhood_life.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../component/appbar_preferred_size.dart';
+import 'components/life_body.dart';
+import 'components/life_header.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('NeighborhoodLifeScreen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text("동네생활"),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.search)),
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle)),
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bell)),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          LifeHeader(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            child: LifeBody(
+              neighborhoodLife: neighborhoodLifeList[0]
+            ),
+          )
+        ],
+      ),
     );
   }
 }
