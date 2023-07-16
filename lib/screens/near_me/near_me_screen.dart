4573786b1b562a9ed3d3feaa341acd603760d889
mungyu <mungyu83@gmail.com>
2023-07-17 01:35:49 +0900

    내 근처 메뉴 - 앱바 및 검색위젯

--- lib/screens/near_me/near_me_screen.dart
+++ lib/screens/near_me/near_me_screen.dart
@@ -1,10 +1,30 @@
+import 'package:carrot_market_ui/screens/component/appbar_preferred_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'components/search_text_field.dart';
 
 class NearMeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('NearMeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('내 근처'),
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.pencil)),
+          IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.bell))
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SearchTextField(),
+          )
+        ],
+      ),
     );
   }
 }
